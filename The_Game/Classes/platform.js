2ff25a9d660cd024e25d1eef7dcbb6cb9d4f80c4
ThatGuyKevin <kevintabiri@gmail.com>
2024-01-05 13:03:03 +0100

    More changes to Platform Class

--- The_Game/Classes/platform.js
+++ The_Game/Classes/platform.js
@@ -1,6 +1,7 @@
 class Platform {
     #Position;
     #Direction;
+    #Colour;
     #InputsActivated = {
         A : false,
         D : false,
@@ -13,9 +14,11 @@ class Platform {
 
     constructor( Width, Height ){
         this.#Position = new Vector2( 200, 200 );
-        this.#Direction = 0;
         this.#Width = Width;
         this.#Height = Height;
+        this.#Colour = `rgb(255, 0, 0)`
+
+        this.#Direction = 0;
     }
 
     get Width(){
@@ -36,7 +39,7 @@ class Platform {
     }   
 
     Update(){
-        Direction = 0;
+        this.#Direction = 0;
 
         for ( const [Input, KeyDown] of Object.entries( this.#InputsActivated ) ){
             if ( !KeyDown ){
@@ -45,25 +48,33 @@ class Platform {
 
             switch ( Input ){
                 case "A":
-                    Direction -= 1;
+                    this.#Direction = -1;
                     break;
                 case "D":
-                    Dircetion += 1;
+                    this.#Direction = 1;
                     break;
                 case "LEFTARROW":
-                    Direction -= 1;
+                    this.#Direction = -1;
                     break;
                 case "RIGHTARROW":
-                    Direction += 1;
+                    this.#Direction = 1;
                     break;
             }
         }
 
-        this.#Position.add( new Vector2( Direction, 0 ) );
+        this.#Position.add( new Vector2(  this.#Direction, 0 ) );
     }
 
 
     Draw(){
-    
+        Position = this.#Position;
+        Width = this.#Width;
+        Height = this.#Height;
+        
+        context.fillStyle = this.#Colour;
+        context.FillRect( 
+            Position.x - Width/2 , Position.y - Height/2,
+            Width, Height  
+        );
     }
 }
