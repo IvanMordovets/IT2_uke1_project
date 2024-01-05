fad86d5449568e14c518f7b293852043980295c8
J3SPER <jeantonsen@elev.rogfk.no>
2024-01-05 13:54:02 +0100

    bricks

--- The_Game/code.js
+++ The_Game/code.js
@@ -5,6 +5,18 @@ let ctx = canvas.getContext("2d");
 let Ball = new ball(new Vector2(200, 200), new Vector2(0.1,0.01), 10, "red");
 let PlatformObject = new Platform( new Vector2( 200, 350 ), 30, 50 );
 
+let numberOfRows = 10;
+let numberOfColumns = 6;
+let rowArray = [];
+let columnArray = [];
+
+for (let c = 0; c < numberOfColumns; c++) {
+    for (let r = 0; r < numberOfRows; r++) {
+        let brick = new Brick(new Vector2(0, 0), ctx.canvas.width / numberOfColumns, 20, "red");
+        rowArray.push(brick);
+    }
+    columnArray.push(brick);
+}
 
 function clearCanvas() {
     ctx.beginPath();
