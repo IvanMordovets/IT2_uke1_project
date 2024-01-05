94b74f397071120dc02640a6caa9b7c26dde5a10
IvanMordovets <imordovets@gmail.com>
2024-01-05 12:31:04 +0100

    ballClass_v1

---
+++ The_Game/ball.js
@@ -0,0 +1,43 @@
+class ball {
+    
+    #position;
+    #velocity;
+    #radius;
+    #color;
+
+    constructor(position, velocity, radius, color) {
+        this.#position = position;
+        this.#velocity = velocity;
+        this.#radius = radius;
+        this.#color = color;
+    }
+
+    get position() {
+        return this.#position;
+    }
+
+    get velocity() {
+        return this.#velocity;
+    }
+
+    checkCollision() {
+
+        //wall collision
+        if(this.#position.x - this.#radius < 0 || this.#position.x + this.#radius > canvas.width) {
+            this.#velocity.flipX();
+        }
+        if(this.#position.y - this.#radius < 0 || this.#position.y + this.#radius > canvas.height) {
+            this.#velocity.flipY();
+        }
+    }
+
+    update( deltaTime ) {
+
+    }
+
+    draw() {
+        ctx.fillStyle = this.#color;
+        ctx.arc(this.#position.x, this.#position.y, this.#radius, 0, 2*Math.PI);
+        ctx.fill();
+    }
+}
