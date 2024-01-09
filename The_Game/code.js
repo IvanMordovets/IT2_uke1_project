396308fa2a1463ac94b8915a3ed7066480841663
IvanMordovets <imordovets@gmail.com>
2024-01-09 08:21:57 +0100

    bricks_made

--- The_Game/code.js
+++ The_Game/code.js
@@ -2,27 +2,34 @@ let canvas = document.getElementById("canvasBreakOut");
 let ctx = canvas.getContext("2d");
 
 
-let Ball = new ball(new Vector2(200, 200), new Vector2(0.1,0.01), 10, "red");
+let Ball = new ball(new Vector2(200, 200), new Vector2(0.1,-0.1), 10, "red");
 
 let PlatformObject = new Platform( new Vector2( 200, 270 ), 95, 20 );
 
-let numberOfRows = 10;
-let numberOfColumns = 6;
+let numberOfRows = 5;
+let numberOfColumns = 5;
+let brickWidth = canvas.width / numberOfRows;
+let brickHeight = canvas.height / numberOfColumns;
 let rowArray = [];
-let columnArray = [];
+let brickArray = [];
 
 for (let c = 0; c < numberOfColumns; c++) {
     for (let r = 0; r < numberOfRows; r++) {
-        let brick = new Brick(new Vector2(0, 0), ctx.canvas.width / numberOfColumns, 20, "red");
-        rowArray.push(brick);
+        let brick = new Brick(new Vector2(brickWidth*r, 20*c), brickWidth, 20, 'rgb(' + Math.random()*255 + ", 100, 100)");
+        brickArray.push(brick);
     }
-    columnArray.push(brick);
+
 }
 
+console.log(brickArray);
 function clearCanvas() {
     ctx.beginPath();
     ctx.fillStyle = "black";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
+
+    brickArray.forEach(brick => {
+        brick.draw();
+    });
 }
 
 function Clamp( min, max, num ){
@@ -47,8 +54,10 @@ function animation(time) {
     Ball.draw();
     PlatformObject.Draw();
     Ball.update( deltaTime );
-    PlatformObject.Update()
     Ball.checkCollision();
+    brickArray = Ball.brickCollision(brickArray);
+    Ball.brickCollision(PlatformObject);
+    PlatformObject.Update()
 
     requestAnimationFrame(animation);
 }
